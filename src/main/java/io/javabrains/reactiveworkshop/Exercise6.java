f7b3d3b3961c20b913cb0dbe8453aa11c1b0ae11
Krishna Choudhary <krishu1501@gmail.com>
2023-08-25 01:36:14 +0530

    rectified exercise 6 solution

--- src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
@@ -2,6 +2,7 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
 public class Exercise6 {
 
@@ -12,14 +13,17 @@ public class Exercise6 {
 
         // Get the value from the Mono into a String variable but give up after 5 seconds
         String value;
+//        value = ReactiveSources.unresponsiveMono().block(Duration.ofSeconds(5));
+
+//        the following does work
 //        value = ReactiveSources.stringMono().block(Duration.ofSeconds(5));
 //        System.out.println(value);
 
-//        value = ReactiveSources.unresponsiveMono().block(Duration.ofSeconds(5));
-
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
         // Come back and do this when you've learnt about operators!
-        value = ReactiveSources.unresponsiveMono().timeout(Duration.ofSeconds(5)).block();
+        List<String> values = ReactiveSources.unresponsiveFlux()
+                .collectList()
+                .block(Duration.ofSeconds(5));
 
         System.out.println("Press a key to end");
 //        System.in.read();
